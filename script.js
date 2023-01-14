308326d189598dd8b37a8c874777027cc93bc30d
Dozzap <78162649+Dozzap@users.noreply.github.com>
2023-01-13 19:20:16 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-window.onload = function() {
-    document.getElementById("about_Me-id").classList.add("show");
-  };
 
+
+document.addEventListener("click", function(){
+  document.body.classList.remove("no-scroll");
+  document.getElementById("landingpage").classList.remove("landingpage");
+  document.getElementById("landingpage").classList.remove("text");
+
+
+});
+
+function scrollToAboutMe() {
+  var thingsIDo = document.getElementById("short_cuts");
+  thingsIDo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+function scrollToThingsIDo() {
+  var thingsIDo = document.getElementById("thingsIDo");
+  thingsIDo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+function scrollToProjects() {
+  var thingsIDo = document.getElementById("thingsToDo");
+  thingsIDo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
+
+
+function scrollToContactMe() {
+  var thingsIDo = document.getElementById("contact");
+  thingsIDo.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+}
